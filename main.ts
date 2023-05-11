d4746114a5c3bf7593cc8ef07dea709fc90cdad4
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2023-05-11 09:26:55 +0200

    auto

--- main.ts
+++ main.ts
@@ -1,25 +1,27 @@
+radio.setGroup(3)
+radio.setFrequencyBand(7)
 
-console.log(String.fromCharCode(64)) //vratí to nějaký znak; 64 je @
-    const data = '@dfhd'
-    console.log("@dfhd".charCodeAt(0))
-    console.log(data.charCodeAt(1))
+let btnA = false
+let btnB = false
 
-function pack(x: number, y: number, z: number): number {
-    let xmod = x + 1024 //posun do klad. intervalu
-    xmod = Math.floor(xmod / 8) // redukce intervalu
-
-    return 0 
-}
-
-input.onButtonPressed(Button.A, function() {
-    let x = 1022; // <-1024; 1023>
-    let y = -950;
-    let z = 12; 
- 
-    //radio.sendString("")
-    radio.sendNumber(pack(x, y, z))
+basic.forever(function() {
+    if (btnA == true) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
+    } else {
+        PCAmotor.MotorStop(PCAmotor.Motors.M1)
+    }
+    if (btnB == true) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 250)
+    } else {
+        PCAmotor.MotorStop(PCAmotor.Motors.M4)
+    }
 })
 
-radio.onReceivedNumber(function(receivedNumber: number) {
-    basic.showNumber(receivedNumber)
+radio.onReceivedValue(function(name: string, value: number) {
+    if (name == "btnA") {
+        btnA = (value == 1)
+    }
+    if (name == "btnB") {
+        btnB = (value == 1)
+    }
 })
